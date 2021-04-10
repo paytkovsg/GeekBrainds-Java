123ba019b4c3c46c627f03c5781543df1e47f712
paytkovsg <paytkov.s@gmail.com>
2021-04-10 23:15:52 +0500

    hw4

---
+++ src/main/java/lesson4/Employee.java
@@ -0,0 +1,13 @@
+package lesson3;
+
+public class Staff {
+    private String surname;
+    private String name;
+    private String patronymic;
+    private String position;
+    private String phone;
+    private float salary;
+    private int age;
+
+    
+}
