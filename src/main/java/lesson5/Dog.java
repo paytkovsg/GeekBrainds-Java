b24aa43a887e4313793b4516af739119b7bfa509
paytkovsg <paytkov.s@gmail.com>
2021-04-15 21:23:03 +0500

    hw5/

--- src/main/java/lesson5/Dog.java
+++ src/main/java/lesson5/Dog.java
@@ -1,2 +1,8 @@
-package lesson5;public class Dog {
+package lesson5;
+
+public class Dog extends Animals {
+    Dog(String name, float maxRun, float maxJump, float maxSwim){
+        super("Собака", name, maxRun, maxJump, maxSwim);
+    }
+
 }
--- src/main/java/lesson5/Horse.java
+++ src/main/java/lesson5/Horse.java
@@ -1,2 +1,7 @@
-package lesson5;public class Horse {
+package lesson5;
+
+public class Horse extends Animals{
+    Horse (String name, float maxRun, float maxJump, float maxSwim){
+        super("Лошадь", name, maxRun, maxJump, maxSwim);
+    }
 }
